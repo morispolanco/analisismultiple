1566c5835b013dc822d351ed81e1173ca0dc7ea3
morispolanco <morispolanco@gmail.com>
2025-03-27 01:56:08 -0600

    Update app.py

--- app.py
+++ app.py
@@ -8,10 +8,23 @@ from nltk.corpus import stopwords
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
 import io
+import os
 
-# Descargar recursos de NLTK
-nltk.download('punkt')
-nltk.download('stopwords')
+# Especificar un directorio personalizado para los datos de NLTK
+nltk_data_path = os.path.join(os.getcwd(), "nltk_data")
+if not os.path.exists(nltk_data_path):
+    os.makedirs(nltk_data_path)
+nltk.data.path.append(nltk_data_path)
+
+# Descargar recursos de NLTK solo si no están presentes
+try:
+    nltk.data.find('tokenizers/punkt')
+except LookupError:
+    nltk.download('punkt', download_dir=nltk_data_path)
+try:
+    nltk.data.find('corpora/stopwords')
+except LookupError:
+    nltk.download('stopwords', download_dir=nltk_data_path)
 
 # Configuración de la página
 st.set_page_config(page_title="Análisis Literario Multidisciplinario", layout="wide")
